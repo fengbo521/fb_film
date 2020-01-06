c0406f904eab4b4889062e2bcf7be8ab23cab939
fb <fb@qq.com>
2020-01-06 10:33:53 +0800

    工程初始化 git commit -m 工程初始化

---
+++ fb_film/src/main/java/com/fb/springboot/FbFilmApplication.java
@@ -0,0 +1,13 @@
+package com.fb.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbFilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbFilmApplication.class, args);
+	}
+
+}
---
+++ fb_film/src/test/java/com/fb/springboot/FbFilmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fb.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FbFilmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
